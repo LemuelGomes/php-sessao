29712f2e193c33c3c4a297e4a7811a596351250a
LEMUEL CAMPOS MENEZES GOMES <lemuel0072004@gmail.com>
2023-07-26 22:12:30 -0300

    Projeto em andamento

--- index.php
+++ index.php
@@ -14,27 +14,28 @@
 </head>
 <body>
 
-<div id="Login">
-    
-    <form>   
-    <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Usuário</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">       
-    </div>
-    
-    <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Senha</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-    </div>
-    
-    <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Termos de Condições</label>
-    </div>
-    
-    <button type="submit" class="btn btn-primary">Entrar</button>
-    </form>
-<div>        
+    <div id="Login">
+        <p id="login-titulo"> Bem-vindo </p>
+        
+        <form action="login.php" method="POST">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">                     
+            </div>
+        
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+        
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Mantenha-me Logado!</label>
+            </div>
+        
+                <button type="submit" class="btn btn-success">Entrar</button>
+                </form>
+    <div>        
     
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+
+$email = "lemuel@gmail.com";
+$senha = "1234";
+
+if($email == InputEmail)
+{
+    header("Location: sistema.php");
+}
+
+if($senha == InputSenha)
+{
+    header("Location: sistema.php");
+}
+
+?>
